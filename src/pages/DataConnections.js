2c326655d48395536a0c28a493b28c34e4a5d785
Suyeon Kim <57708995+syk001108@users.noreply.github.com>
2024-08-14 20:04:07 +0900

    FEAT: Update DataConnections.js

--- src/pages/DataConnections.js
+++ src/pages/DataConnections.js
@@ -4,14 +4,27 @@ import Navbar from '../Navbar';
 import './DataConnections.css';
 
 export const DataConnections = () => {
+  const handleConnectionClick = (type) => {
+    console.log(`${type} button clicked`);
+    // Add functionality for button click here
+  };
+
   return (
     <div className="d-flex E">
-      <div><Sidebar /></div>
-      <div style={{ flex: '1 1 auto', display: 'flex', flexFlow: 'column', height: '100vh', overflowY: 'hidden' }}>
+      <div className="sidebar"><Sidebar /></div>
+      <div className="main-content">
         <Navbar />
         <div className="event-content">
-          <div className="event-header" style={{ display: 'flex', alignItems: 'center' }}>
-          
+          <div className="event-header">
+            <h3 className='main-header'>Vendor</h3>
+            <div className="connection-box" onClick={() => handleConnectionClick('MySQL')}>
+              <img src="/images/mysql-logo.png" alt="MySQL" className="logo" />
+              <div className="connection-text">MySQL</div>
+            </div>
+            <div className="connection-box" onClick={() => handleConnectionClick('PostgreSQL')}>
+              <img src="/images/elephant.png" alt="PostgreSQL" className="logo" />
+              <div className="connection-text">PostgreSQL</div>
+            </div>
           </div>
         </div>
       </div>
